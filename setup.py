20f12b73ac68c9b29f5fcdfb0e3a437aa8b1c115
Stefan Jacholke <stefanjacholke@gmail.com>
2018-06-08 21:07:25 +0200

    Add setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='PyWaves',
+      version='1.0',
+      description='PyWaves',
+      author='PyWaves',
+      author_email=' ',
+      url='https://github.com/PyWaves/PyWaves',
+      packages=['distutils', 'distutils.command'],
+     )
